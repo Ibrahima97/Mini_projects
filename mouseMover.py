366264830baddb0cc955aa61d60326cc3a76a094
Ibrahima97 <ibrahimgb97@gmail.com>
2022-11-21 09:44:13 +0000

    mouse

---
+++ mouseMover.py
@@ -0,0 +1,7 @@
+import mouse
+import time
+while (True):
+     mouse.move(2830,350,absolute=True,duration=3)
+     time.sleep(3)
+     mouse.move(2730,450,absolute=True,duration=3)
+     time.sleep(3)
